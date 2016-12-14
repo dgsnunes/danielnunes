8158a0125c185780cb667f08124b487b10b15891
dgsnunes <daniel.silva.nunes@gmail.com>
2016-12-14 11:00:49 -0200

    Correção do route do Aluno

--- routes/aluno.js
+++ routes/aluno.js
@@ -2,43 +2,43 @@ var express = require('express');
 var router = express.Router();
 
 var mongoose = require('mongoose');
-var Usuario = require('../models/Usuario.js');
+var Aluno = require('../models/Aluno.js');
 
-/* GET /usuario Listagem de usuários. */
+/* GET /aluno Listagem de usuários. */
 router.get('/', function(req, res, next) {
-  Usuario.find(function (err, usuario) {
+  Aluno.find(function (err, aluno) {
     if (err) return next(err);
-    res.json(usuario);
+    res.json(aluno);
   });
 });
 
-/* POST /usuario Cadastro de usuário */
+/* POST /aluno Cadastro de usuário */
 router.post('/', function(req, res, next) {
-  Usuario.create(req.body, function (err, post) {
+  Aluno.create(req.body, function (err, post) {
     if (err) return next(err);
     res.json(post);
   });
 });
 
-/* GET /usuario/id  Lista filtrada por um usuário*/
+/* GET /aluno/id  Lista filtrada por um usuário*/
 router.get('/:id', function(req, res, next) {
-  Usuario.findById(req.params.id, function (err, post) {
+  Aluno.findById(req.params.id, function (err, post) {
     if (err) return next(err);
     res.json(post);
   });
 });
 
-/* PUT /usuario/:id Salva a edição de usuário */
+/* PUT /aluno/:id Salva a edição de usuário */
 router.put('/:id', function(req, res, next) {
-  Usuario.findByIdAndUpdate(req.params.id, req.body, function (err, post) {
+  Aluno.findByIdAndUpdate(req.params.id, req.body, function (err, post) {
     if (err) return next(err);
     res.json(post);
   });
 });
 
-/* DELETE /usuario/:id Deletando o usuário a partir do id */
+/* DELETE /aluno/:id Deletando o usuário a partir do id */
 router.delete('/:id', function(req, res, next) {
-  Usuario.findByIdAndRemove(req.params.id, req.body, function (err, post) {
+  Aluno.findByIdAndRemove(req.params.id, req.body, function (err, post) {
     if (err) return next(err);
     res.json(post);
   });
